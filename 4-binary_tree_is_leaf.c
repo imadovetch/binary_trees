551f04a761313e7debbec40a19ea29c1be532598
imadovetch <ibouderoua63@gmail.com>
2023-11-28 21:00:39 +0100

    task leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_is_leaf - base.
+ * @node: arg one.
+ * Return: boolean.
+ */
+
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+
+
+	return (1);
+}
