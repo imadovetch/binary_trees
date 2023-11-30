bbc7123ff552cdb73b3be3245cd7ce62ab143990
imadovetch <ibouderoua63@gmail.com>
2023-11-30 22:21:22 +0100

    tasks after chek

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,18 +8,18 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t lv = 0;
-
+	size_t leaves = 0;
 
 	if (tree)
 	{
-
 		if (tree->left == NULL && tree->right == NULL)
-			lv = 1;
-		lv += binary_tree_lv(tree->right);
-		lv += binary_tree_lv(tree->left);
+			leaves = 1;
 
+
+
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
 	}
 
-	return (lv);
+	return (leaves);
 }
